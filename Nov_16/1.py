92786240175c6743b09bda017de72ed324d2c3ab
fumiko <2394425147@qq.com>
2023-11-16 14:49:33 +0800

    feat: Finished assignments for Nov 16

---
+++ Nov_16/1.py
@@ -0,0 +1,35 @@
+import math
+
+def is_prime(number: int) -> bool:
+    if number <= 1:
+        return False
+
+    if number == 2 or number == 3:
+        return True
+
+    if number % 2 == 0 or number % 3 == 0:
+        return False
+
+    # 大于3的所有素数都能表达为6k+/-1 (k为整数)
+    evaluator = 5
+    number_sqrt = math.sqrt(number)
+
+    while evaluator <= number_sqrt:
+        if number % evaluator == 0 or number % (evaluator + 2) == 0:
+            return False
+        
+        evaluator += 6
+
+    return True
+
+
+while True:
+    number = int(input("请输入一个大于0的整数: "))
+
+    if number == 0:
+        break
+
+    if is_prime(number):
+        print("是素数")
+    else:
+        print("不是素数")
---
+++ Nov_16/2.py
@@ -0,0 +1,27 @@
+import math
+
+def GCD_function(number1: int, number2: int) -> (int, int):
+    # calculate gcd
+    swizzle1 = number1
+    swizzle2 = number2
+    while(swizzle2):
+        swizzle1, swizzle2 = swizzle2, swizzle1 % swizzle2
+    gcd = swizzle1
+    
+    # calculate lcm
+    # proof: https://math.stackexchange.com/questions/349858/easiest-and-most-complex-proof-of-gcd-a-b-times-operatornamelcm-a-b-a
+    lcm = (number1 * number2) // gcd
+    
+    return gcd, lcm
+
+while True:
+    number1 = int(input("请输入第一个数字(输入0结束): "))
+    if number1 == 0:
+        break
+
+    number2 = int(input("请输入第二个数字(输入0结束): "))
+    if number2 == 0:
+        break
+
+    gcd, lcm = GCD_function(number1, number2)
+    print(f"最大公约数: {gcd}\n最小公倍数: {lcm}\n")
