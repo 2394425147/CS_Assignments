0fd4e0f6099ded7e3a215e49a9a5cc155e9e1e2d
fumiko <2394425147@qq.com>
2024-01-04 14:04:04 +0800

    bugfix: Packed assignment nesting

--- Utillities/pack_assignment.py
+++ Utillities/pack_assignment.py
@@ -1,8 +1,11 @@
 import os
 import shutil
+import readline
 
 os.chdir(os.path.dirname(os.path.dirname(__file__)))
 
+readline.set_completer_delims(' \t\n=')
+readline.parse_and_bind("tab: complete")
 folder = input("Folder to pack: ")
 
 if not os.path.exists(folder):
@@ -16,7 +19,8 @@ export_name = f"./1230283057_陈星凯_实验{experiment_number}"
 if os.path.exists(export_name):
     os.system(f"rm -rf {export_name}")
 
-os.system(f"cp -r {folder} {export_name}")
+os.mkdir(f"{export_name}")
+os.system(f"cp -r {folder} {export_name}/{export_name}")
 
 shutil.make_archive(export_name, "zip", export_name)
 
